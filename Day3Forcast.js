01d277c3b16dd089297cdf8cba8c26455fe088ce
Mamessile Kiman <sile.kiman@yahoo.com>
2019-11-24 23:34:11 -0500

    adding more code to 5 days forecast

---
+++ Day3Forcast.js
@@ -0,0 +1,120 @@
+ //Function to append  day3 forecast
+function day3Forcast(history) {
+    var history = localStorage.getItem("City Name");
+
+    if (history === null) {
+        return;
+
+    } else {
+
+        $(".history").prepend("<h5>" + history + "</h5>" + "<br />")
+    }
+    //Set the date 
+    var date = new Date();
+    date = moment().format('L');
+    var currentTime = moment().format("H");
+    // This is my API key
+    var APIKey = "eeab2a767f4b39347cacd521da7d158c";
+
+    // Here I'm building the URL we need to query the database
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + history + ",us" + "&appid=" + APIKey;
+
+    // Here I'm running   AJAX call to the OpenWeatherMap API
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        // Im storing all of the retrieved data inside of an object called "response"
+        .then(function (response) {
+
+            //set icon URL day time
+            var sunnyDayURL = "http://openweathermap.org/img/wn/01d.png";
+            var rainyDayURL = "http://openweathermap.org/img/wn/10d.png";
+            var cloudDayURL = "http://openweathermap.org/img/wn/02d.png";
+
+            //set icon URL night time
+            var rainyNightURL = "http://openweathermap.org/img/wn/10n.png";
+            var nightURL = "http://openweathermap.org/img/wn/01n.png";
+            var cloudNightURL = "http://openweathermap.org/img/wn/02n.png";
+            var snowURL = "http://openweathermap.org/img/wn/13d.png";
+
+            //set weather condition variable
+            var weatherCondition = response.list[17].weather[0].main;
+            //create an image element to append the icons
+            var image = $("<img>");
+
+
+            //Set conditional statement to display the weather icon during snow time 
+            if (weatherCondition === "Snow") {
+                image.attr("src", snowURL);
+
+
+            } else {
+
+
+                //Set conditional statement to display the weather icon during day time  
+                if (currentTime <= 17 && weatherCondition === "Clouds") {
+                    image.attr("src", cloudDayURL);
+
+                } else if (currentTime <= 17 && weatherCondition === "Rain") {
+                    image.attr("src", rainyDayURL);
+
+                } else if (currentTime <= 17 && weatherCondition === "Clear") {
+                    image.attr("src", sunnyDayURL);
+                }
+                //This is night time condition statement
+                if (currentTime >= 18 && weatherCondition === "Clouds") {
+                    image.attr("src", cloudNightURL);
+
+                } else if (currentTime >= 18 && weatherCondition === "Rain") {
+                    image.attr("src", rainyNightURL);
+
+                } else if (currentTime >= 18 && weatherCondition === "Clear") {
+                    image.attr("src", nightURL);
+                }
+            }
+
+            // //Convert the temparature into F
+            var tempF = (response.list[17].main.temp - 273.15) * 1.80 + 32;
+            //truncate it to 2 digits
+            var temparature = tempF.toFixed(2);
+            //Set humidity variable and content
+            var humidity = response.list[17].main.humidity;
+
+            //Set date variable and content 
+            var Date1 = response.list[17].dt_txt
+            date1 = moment(Date1).format('L');
+
+            //create another div to hold all the 5 days forcast
+            var daysEl = $("#day3");
+            daysEl.attr("style", "color:white")
+            daysEl.attr("style", "background: royalblue")
+            daysEl.css("border", "0.5px solid gray ");
+            daysEl.css("margin", "5px");
+            daysEl.css("padding", "5px");
+            daysEl.css("border-radius", "5px");
+
+
+            // //display the day 1 in a h6  tag appended to the div col-2
+            var d1El = $("<h6>");
+            d1El.text(date1);
+            daysEl.append(d1El);
+
+            // //display the icon in an li tag appended to the div container
+            var icon = $("<p>");
+            icon.append(image);
+            daysEl.append(icon);
+
+            //display the Temparature in a p  tag appended to the div col-2
+            var temp = $("<p>");
+            temp.text("Temp: " + temparature + " °F");
+            daysEl.append(temp);
+
+            //display the Humidity in an p tag appended to the div container
+            var Humidity = $("<p>");
+            Humidity.text("Humidity: " + humidity + " %");
+            daysEl.append(Humidity);
+
+        })
+
+}
--- weather.js
+++ weather.js
@@ -17,8 +17,129 @@ searchbtn.addEventListener("click", function (event) {
     }
     //call the function the last item from local storage
     //This fuction also performs the function that dispalay all the weather conditions
-    displayHistory(history)
+    retrieveAPI(searchVal)
+    dailyForcast()
      
 });
+//function retrieve data from the weather API and populate the Temp and wheather conditions on the page.  
+function retrieveAPI(searchVal) {
+    //Set the date 
+    var date = new Date();
+    date = moment().format('L');
+    var currentTime = moment().format("H");
+    console.log(currentTime)
+    // This is my API key
+    var APIKey = "eeab2a767f4b39347cacd521da7d158c";
+
+    // Here I'm building the URL we need to query the database
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+        "q=" + searchVal + "&appid=" + APIKey;
+
+    // Here I'm running   AJAX call to the OpenWeatherMap API
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        // Im storing all of the retrieved data inside of an object called "response"
+        .then(function (response) {
+
+            //set icon URL day time
+            var sunnyDayURL = "http://openweathermap.org/img/wn/01d@2x.png";
+            var rainyDayURL = "http://openweathermap.org/img/wn/10d@2x.png";
+            var cloudDayURL = "http://openweathermap.org/img/wn/02d@2x.png";
+
+            //set icon URL night time
+            var rainyNightURL = "http://openweathermap.org/img/wn/10n@2x.png";
+            var nightURL = "http://openweathermap.org/img/wn/01n@2x.png";
+            var cloudNightURL = "http://openweathermap.org/img/wn/02n@2x.png";
+            var snowURL = "http://openweathermap.org/img/wn/13d@2x.png";
+
+            //set weather condition variable
+            var weatherCondition = response.weather[0].main;
+            //create an image element to append the icons
+            var image = $("<img>");
+
+
+            //Set conditional statement to display the weather icon during snow time 
+            if (weatherCondition === "Snow") {
+                image.attr("src", snowURL);
+
+
+            } else {
+
+
+                //Set conditional statement to display the weather icon during day time  
+                if (currentTime <= 17 && weatherCondition === "Clouds") {
+                    image.attr("src", cloudDayURL);
+
+                } else if (currentTime <= 17 && weatherCondition === "Rain") {
+                    image.attr("src", rainyDayURL);
+
+                } else if (currentTime <= 17 && weatherCondition === "Clear") {
+                    image.attr("src", sunnyDayURL);
+                }
+                //This is night time condition statement
+                if (currentTime >= 18 && weatherCondition === "Clouds") {
+                    image.attr("src", cloudNightURL);
+
+                } else if (currentTime >= 18 && weatherCondition === "Rain") {
+                    image.attr("src", rainyNightURL);
+
+                } else if (currentTime >= 18 && weatherCondition === "Clear") {
+                    image.attr("src", nightURL);
+                }
+            }
+
+            //Convert the temparature into F
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            //truncate it to 2 digits
+            var temparature = tempF.toFixed(2);
+            //Set humidity variable and content
+            var humidity = response.main.humidity
+            //Set wind variable and content
+            var wind = response.wind.speed
+            //Create a new Div and set its attribute to append all the weather items   
+            var weather = $("<div>");
+            weather.addClass(".displayCity")
+            weather.css("border", "0.5px solid gray ");
+            weather.css("margin", "5px");
+            weather.css("padding", "5px");
+            weather.css("border-radius", "5px");
+
+            //display the City and date in h4 tag appended to the div container
+            var cityEl = $("<h4>");
+            cityEl.addClass("city");
+            cityEl.attr("margin:10px")
+            cityEl.text(searchVal + "(" + date + ")");
+            cityEl.append(image)
+            weather.append(cityEl);
+
+            //display the Temparature in a li  tag appended to the div container
+            var temp = $("<p>");
+            temp.addClass("tempature");
+            temp.text("Temparature: " + temparature + " °F");
+            weather.append(temp);
+
+            //display the Humidity in an li tag appended to the div container
+            var Humidity = $("<p>");
+            Humidity.addClass("humidity");
+            Humidity.text("Humidity: " + humidity + " %")
+            weather.append(Humidity);
+
+            //display the Wind in an li tag appended to the div container
+            var Wind = $("<p>");
+            Wind.addClass("humidity");
+            Wind.text("Wind-Speed:  " + wind + " mph")
+            weather.append(Wind);
+
+            //prepend all the city weather information to the div main container
+            $(".col-sm-8").prepend(weather);
+             
+
+        })
+        //dailyForcast()
+}
+
+
 
  
